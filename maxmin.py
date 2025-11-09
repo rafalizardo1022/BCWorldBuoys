813991c6f827c412cfd9acafbd4a2de4a08965c6
rafalizardo1022 <rafaellizardo1022@gmail.com>
2025-11-08 23:45:29 -0500

    Add comments to maxmin.py for clarity
    Added comments to explain the purpose of the code.

--- maxmin.py
+++ maxmin.py
@@ -1,3 +1,5 @@
+#Code to get the exact MIN and MAX LAT and LONGS of your TIF file
+
 import rasterio
 
 tif_path = r"C:\Users\Rafa\Documents\GitHub\bc-world\GMRT_OSM_Importer\GMRTv4_4_0_20251021topo.tif"
@@ -6,3 +8,4 @@ with rasterio.open(tif_path) as src:
     b = src.bounds
     print(f"MIN_LAT, MAX_LAT = {b.bottom:.6f}, {b.top:.6f}")
     print(f"MIN_LON, MAX_LON = {b.left:.6f}, {b.right:.6f}")
+
